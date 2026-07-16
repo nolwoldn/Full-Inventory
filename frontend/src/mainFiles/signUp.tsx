5ca9f5b15731151f772331e132db2833b02207da
Godusop46 <sileshinolan@gmail.com>
2026-07-17 00:21:38 +0300

    added some html to front end

--- frontend/src/mainFiles/signUp.tsx
+++ frontend/src/mainFiles/signUp.tsx
@@ -1,11 +1,10 @@
-import React,{ useState, useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 
-interface confirmPassObject {
-  passMatch: boolean;
-  selected: boolean;
+interface passwordObject {
+  password: string;
+  passwordValid: boolean;
+  passwordSelected: boolean;
 }
-
 interface validatingObject {
   validator: string;
   message: string;
@@ -16,14 +15,20 @@ function Signup() {
     document.title = "Sign up";
   }, []);
 
-  const userEmail = useRef("");
-  const userPassword  = useRef("")
-  const userConfirmPass  = useRef("");
-
-  const [passwordsMatch, changeisMatch] = useState<confirmPassObject>({
-    passMatch: true,
-    selected: false,
+  const userEmail = useRef(null);
+  const [userPassword, changeUserPassword] = useState<passwordObject>({
+    password: "",
+    passwordValid: true,
+    passwordSelected: false,
   });
+  const [userConfirmPass, changeUserConfrimPassword] = useState<passwordObject>(
+    {
+      password: "",
+      passwordValid: true,
+      passwordSelected: false,
+    },
+  );
+
   const [userPasswordErrors, changeUserPasswordErrors] = useState<string[]>([]);
 
   const passWordValidationReg: validatingObject[] = [
@@ -42,30 +47,90 @@ function Signup() {
     },
   ];
 
-  const checkPasswords = (): void => {
-    const isMatch = userConfirmPass.current === userPassword.current;
-
-    changeisMatch({ passMatch: isMatch, selected: passwordsMatch.selected });
+  const checkPasswords = (confirmPass: string): boolean => {
+    const isMatch = confirmPass === userPassword.password;
+    return isMatch;
   };
 
-  const validatePassword = () : void => {
+  const validatePassword = (): boolean => {
+    let passWordErrors: string[] = [];
     for (let i of passWordValidationReg) {
-     
-      let passWordErrors: string[] = [];
-      if (!i.validator.match(userPassword.current)) {
+      if (!i.validator.match(userPassword.password)) {
         passWordErrors.push(i.message);
       }
-
-      if (passWordErrors.length > 0) {
-        changeUserPasswordErrors(passWordErrors);
-      }
     }
+    changeUserPasswordErrors(passWordErrors);
+    return passWordErrors.length > 0;
   };
-
   return (
-    <div className="sign-up-full">
-      <div className="sign-up-form">
-        
+    <div className="signup-full">
+      <div className="full-signup">
+        <div className="signup-content"></div>
+        <div className="signup-form">
+          <form className="sign-main-form">
+            <label className="signup-labels" htmlFor="userEmail">
+              Your Email
+            </label>
+            <input
+              type="text"
+              id="userEmail"
+              className="signup-form-inputs"
+              ref={userEmail}
+              required
+            />
+
+            <label htmlFor="userPassword" className="signup-labels">
+              Password
+            </label>
+            <input
+              type="password"
+              id="userPassword"
+              onChange={(e) => {
+                changeUserPassword({
+                  password: e.target.value,
+                  passwordValid: validatePassword(),
+                  passwordSelected: true,
+                });
+              }}
+              onBlur={(e) => {
+                changeUserPassword({
+                  password: e.target.value,
+                  passwordValid: validatePassword(),
+                  passwordSelected: false,
+                });
+              }}
+              className="signup-form-inputs"
+              required
+            />
+            {userPassword.passwordSelected && !userPassword.passwordValid && (
+              <div className="password-errors"></div> // TODO: add the system for showing errors
+            )}
+
+            <label htmlFor="userCPass" className="signup-labels">
+              Confirm password
+            </label>
+            <input
+              type="password"
+              className="signup-form-inputs"
+              onChange={(e) => {
+                changeUserConfrimPassword({
+                  password: e.target.value,
+                  passwordValid: checkPasswords(e.target.value),
+                  passwordSelected: true,
+                });
+              }}
+              onBlur={(e) => {
+                changeUserConfrimPassword({
+                  password: e.target.value,
+                  passwordValid: checkPasswords(e.target.value),
+                  passwordSelected: false,
+                });
+              }}
+              required
+            />
+            {/* add system for knowing wheather cPass is same as pass */}
+          </form>
+        </div>
       </div>
     </div>
   );
